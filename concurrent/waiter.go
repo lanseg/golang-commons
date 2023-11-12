296b49c07adc796a78797564bd11a3986d9e8f9a
lans <>
2023-11-12 08:19:52 +0000

    Added some basic tests for the waiters

--- concurrent/waiter.go
+++ concurrent/waiter.go
@@ -8,62 +8,67 @@ import (
 
 const (
 	defaultWaiterInterval = 1 * time.Second
-	defaultWaiterRetries  = 30
+	defaultWaiterRetries  = 10
 )
 
 type ValueProvider[T any] func() optional.Optional[T]
 type Condition[T any] func(optional.Optional[T]) bool
 
-type waiter[T any] struct {
+func NewIsPresent[T any]() Condition[T] {
+	return func(o optional.Optional[T]) bool {
+		return o.IsPresent()
+	}
+}
+
+func NewEquals[T comparable](value T) Condition[T] {
+	return func(o optional.Optional[T]) bool {
+		return o.Filter(func(v T) bool {
+			return v == value
+		}).IsPresent()
+	}
+}
+
+type Waiter[T any] struct {
 	retryCount int
 	interval   time.Duration
 	provider   ValueProvider[T]
 	condition  Condition[T]
 }
 
-func (w *waiter[T]) Wait() optional.Optional[T] {
+func (w *Waiter[T]) Wait() optional.Optional[T] {
 	ticker := time.NewTicker(w.interval)
 	for i := 0; i < w.retryCount; i++ {
 		select {
 		case <-ticker.C:
 			value := w.provider()
 			if w.condition(value) {
-                ticker.Stop()
+				ticker.Stop()
 				return value
 			}
 		}
 	}
+	ticker.Stop()
 	return optional.Nothing[T]{}
-
-}
-
-func newWaiter[T any]() *waiter[T] {
-	return &waiter[T]{
-		retryCount: defaultWaiterRetries,
-		interval:   defaultWaiterInterval,
-	}
 }
 
 // WaitForSomething blocks until src returns optional with value or with error.
 // By default, it tries 30 times with 1 second interval between retries
 func WaitForSomething[T any](src ValueProvider[T]) optional.Optional[T] {
-	waiter := newWaiter[T]()
-	waiter.provider = src
-	waiter.condition = func(o optional.Optional[T]) bool {
-		return o.IsPresent()
-	}
-	return waiter.Wait()
+	return (&Waiter[T]{
+		condition:  NewIsPresent[T](),
+		provider:   src,
+		interval:   defaultWaiterInterval,
+		retryCount: defaultWaiterRetries,
+	}).Wait()
 }
 
 // WaitForValue blocks until src returns optional with given value or with error.
 // By default, it tries 30 times with 1 second interval between retries
 func WaitForValue[T comparable](value T, src ValueProvider[T]) {
-	waiter := newWaiter[T]()
-	waiter.provider = src
-	waiter.condition = func(o optional.Optional[T]) bool {
-		return o.Filter(func(v T) bool {
-			return v == value
-		}).IsPresent()
-	}
-	waiter.Wait()
+	(&Waiter[T]{
+		condition:  NewEquals[T](value),
+		provider:   src,
+		interval:   defaultWaiterInterval,
+		retryCount: defaultWaiterRetries,
+	}).Wait()
 }
--- concurrent/waiter_test.go
+++ concurrent/waiter_test.go
@@ -1,9 +1,76 @@
 package concurrent
 
 import (
-  "testing"
+	"testing"
+	"time"
+
+	"github.com/lanseg/golang-commons/optional"
 )
 
-func TestWaiter(t *testing.T) {
-// TODO: add tests
+const (
+	testInterval = 100 * time.Millisecond
+	testRetries  = 10
+)
+
+func newValueProvider[T any](value T) ValueProvider[T] {
+	return func() optional.Optional[T] {
+		return optional.Of(value)
+	}
+}
+
+func newValueAfterRetries[T any](value T, setAfter int) ValueProvider[T] {
+    return func() optional.Optional[T] {
+        setAfter--
+        if setAfter <= 0 {
+          return optional.Of[T](value)
+        }
+        return optional.Nothing[T]{}
+    }
+}
+
+func newNothingProvider[T any]() ValueProvider[T] {
+	return func() optional.Optional[T] {
+		return optional.Nothing[T]{}
+	}
+}
+
+func newWaiter[T any](condition Condition[T], provider ValueProvider[T]) *Waiter[T] {
+	return &Waiter[T]{
+		interval:   testInterval,
+		retryCount: testRetries,
+		provider:   provider,
+		condition:  condition,
+	}
+}
+
+func TestWaitForSomething(t *testing.T) {
+
+	t.Run("successful run got no value", func(t *testing.T) {
+		result := newWaiter[string](
+			NewIsPresent[string](),
+			newNothingProvider[string]()).Wait()
+	    if result.IsPresent() {
+            t.Errorf("Expected result not to be present, but got %v", result)
+        }
+    })
+
+    t.Run("successful run got a value immediately", func(t *testing.T) {
+         result := newWaiter[string](
+             NewIsPresent[string](),
+             newValueProvider[string]("SomeValue")).Wait()
+         value, _ := result.Get()
+         if value != "SomeValue" {
+             t.Errorf("Expected to get value, but got %v", result)
+         }
+    })
+
+    t.Run("successful run got a value after retries", func(t *testing.T) {
+         result := newWaiter[string](
+             NewIsPresent[string](),
+             newValueAfterRetries[string]("SomeValue", 4)).Wait()
+         value, _ := result.Get()
+         if value != "SomeValue" {
+             t.Errorf("Expected to get value, but got %v", result)
+         }
+    })
 }
