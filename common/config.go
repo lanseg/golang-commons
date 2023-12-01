fd66a7cc7f15cf9e22eb920ca7877879cff20dd4
lans <>
2023-12-01 20:01:45 +0000

    Added simple config for flags/config file

---
+++ common/config.go
@@ -0,0 +1,91 @@
+package common
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"reflect"
+)
+
+type fieldValue struct {
+	field reflect.Value
+	value interface{}
+}
+
+// GetConfig loads configuration defined by type T.
+// Initial configuration is loaded from file defined by the "config" flag,
+// File values could be overridden by the command line flags.
+func GetConfig[T any](args []string, configFile *string) (*T, error) {
+	fs := flag.NewFlagSet("config flags", flag.ContinueOnError)
+	config := new(T)
+
+	if configFile != nil && *configFile != "" {
+		if err := loadConfigFromFile(config, *configFile); err != nil {
+			return nil, err
+		}
+	}
+
+	fieldFlags, err := defineConfigFlags[T](fs, config)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := fs.Parse(args); err != nil {
+		return nil, err
+	}
+
+	fs.Visit(func(f *flag.Flag) {
+		fv := fieldFlags[f.Name]
+		fv.field.Set(reflect.ValueOf(fv.value))
+	})
+	return config, nil
+}
+
+func loadConfigFromFile[T any](cfg *T, configFile string) error {
+	data, err := os.ReadFile(configFile)
+	if err != nil {
+		return err
+	}
+	if err = json.Unmarshal(data, cfg); err != nil {
+		return err
+	}
+	return nil
+}
+
+func defineConfigFlags[T any](fs *flag.FlagSet, config *T) (map[string]*fieldValue, error) {
+	elem := reflect.ValueOf(config).Elem()
+	typeDef := elem.Type()
+	fieldByName := map[string]*fieldValue{}
+
+	for i := 0; i < typeDef.NumField(); i++ {
+		field := elem.Field(i)
+		kind := field.Type().Kind()
+		name := typeDef.Field(i).Name
+		if kind == reflect.Ptr {
+			kind = field.Type().Elem().Kind()
+		}
+		fv := &fieldValue{field: field}
+		fieldByName[name] = fv
+		switch kind {
+		case reflect.Bool:
+			fv.value = fs.Bool(name, false, fmt.Sprintf("Flag for parameter %q", name))
+		case reflect.String:
+			fv.value = fs.String(name, "", fmt.Sprintf("Flag for parameter %q", name))
+		case reflect.Int:
+			fv.value = fs.Int(name, 0, fmt.Sprintf("Flag for parameter %q", name))
+		case reflect.Int64:
+			fv.value = fs.Int64(name, 0, fmt.Sprintf("Flag for parameter %q", name))
+		case reflect.Uint:
+			fv.value = fs.Uint(name, 0, fmt.Sprintf("Flag for parameter %q", name))
+		case reflect.Uint64:
+			fv.value = fs.Uint64(name, 0, fmt.Sprintf("Flag for parameter %q", name))
+		case reflect.Float64:
+			fv.value = fs.Float64(name, 0, fmt.Sprintf("Flag for parameter %q", name))
+		default:
+			return nil, fmt.Errorf("Unsupported field %s of type %s", name, kind)
+		}
+	}
+
+	return fieldByName, nil
+}
---
+++ common/config_test.go
@@ -0,0 +1,99 @@
+package common
+
+import (
+	"fmt"
+	"path/filepath"
+	"reflect"
+	"testing"
+)
+
+type Config struct {
+	StringField        *string `json:"StringField"`
+	AnotherStringField *string `json:"AnotherStringField"`
+	IntegerField       *int    `json:"IntegerField"`
+}
+
+func (c *Config) String() string {
+	return fmt.Sprintf("Config {%q %q %d}", *(c.StringField), *(c.AnotherStringField), *(c.IntegerField))
+}
+
+func ptr[T any](value T) *T {
+	result := new(T)
+	*result = value
+	return result
+}
+
+func TestConfig(t *testing.T) {
+
+	for _, tc := range []struct {
+		name string
+		file string
+		args []string
+		want interface{}
+	}{
+		{
+			name: "flag only config",
+			args: []string{
+				"--StringField=SomeValue",
+				"--AnotherStringField=/another/string/field/",
+				"--IntegerField=123",
+			},
+			want: &Config{
+				StringField:        ptr("SomeValue"),
+				AnotherStringField: ptr("/another/string/field/"),
+				IntegerField:       ptr(123),
+			},
+		},
+		{
+			name: "file only config",
+			file: "sample_config.json",
+			args: []string{},
+			want: &Config{
+				StringField:        ptr("Whatever"),
+				AnotherStringField: ptr("Anyway"),
+				IntegerField:       ptr(12345678),
+			},
+		},
+		{
+			name: "all flags and file config",
+			file: "sample_config.json",
+			args: []string{
+				"--StringField=SomeValue",
+				"--AnotherStringField=/another/string/field/",
+				"--IntegerField=123",
+			},
+			want: &Config{
+				StringField:        ptr("SomeValue"),
+				AnotherStringField: ptr("/another/string/field/"),
+				IntegerField:       ptr(123),
+			},
+		},
+		{
+			name: "some flags and file config",
+			file: "sample_config.json",
+			args: []string{
+				"--StringField=SomeValue",
+				"--IntegerField=123",
+			},
+			want: &Config{
+				StringField:        ptr("SomeValue"),
+				AnotherStringField: ptr("Anyway"),
+				IntegerField:       ptr(123),
+			},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			var tcfile *string
+			if tc.file != "" {
+				tcfile = ptr(filepath.Join("testdata", tc.file))
+			}
+			config, err := GetConfig[Config](tc.args, tcfile)
+			if err != nil {
+				t.Errorf("Unexpected error: %s", err)
+			} else if !reflect.DeepEqual(tc.want, config) {
+				t.Errorf("Expected config for file %q and flags %q should be %v, but got %v",
+					tc.file, tc.args, tc.want, config)
+			}
+		})
+	}
+}
