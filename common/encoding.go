453fa51488aebf48003189ab7cf8931168fcd9d8
lans <>
2023-12-13 12:12:32 +0000

    Add some simple tests for json

--- common/encoding.go
+++ common/encoding.go
@@ -2,7 +2,17 @@ package common
 
 import (
 	"encoding/json"
+	"os"
 )
+
+// FromJsonFile decodes json file into an object
+func FromJsonFile[T any](path string) (*T, error) {
+	bytes, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	return FromJson[T](bytes)
+}
 
 // FromJson just tries to convert json string to object
 func FromJson[T any](data []byte) (*T, error) {
--- common/encoding_test.go
+++ common/encoding_test.go
@@ -1,1 +1,43 @@
 package common
+
+import (
+	"reflect"
+	"testing"
+)
+
+type someStruct struct {
+	SomeInt    int    `json:"some_int"`
+	SomeString string `json:"some_string"`
+}
+
+func TestFromJson(t *testing.T) {
+
+	for _, tc := range []struct {
+		name    string
+		json    string
+		want    *someStruct
+		wantErr bool
+	}{
+		{
+			name: "normal json",
+			json: "{\"some_int\": 1234, \"some_string\": \"abcd\"}",
+			want: &someStruct{1234, "abcd"},
+		},
+		{
+			name:    "broken json",
+			json:    "{wefwef",
+			wantErr: true,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := FromJson[someStruct]([]byte(tc.json))
+			if (err != nil && !tc.wantErr) || (err == nil && tc.wantErr) {
+				t.Errorf("Unexpected error or missing an expected error: %v", err)
+			}
+
+			if !reflect.DeepEqual(result, tc.want) {
+				t.Errorf("expected FromJson to return %s, but got %s", tc.want, result)
+			}
+		})
+	}
+}
