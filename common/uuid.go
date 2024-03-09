a5e5d24d6452ab01607e3052ce91405867199833
lans <>
2024-03-09 17:04:30 +0000

    Added uuid generator for any interface

--- common/uuid.go
+++ common/uuid.go
@@ -3,16 +3,34 @@ package common
 import (
 	"crypto/rand"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
+	"hash/fnv"
 )
 
-func UUID4() string {
-	bytes := make([]byte, 16)
-	rand.Read(bytes)
+func uuid4ForBytes(bytes []byte) string {
 	bytes[6] = byte(0x40 | (int(bytes[6]) & 0xf))
 	bytes[8] = byte(0x80 | (int(bytes[8]) & 0x3f))
 	result := hex.EncodeToString(bytes)
 
 	return fmt.Sprintf("%s-%s-%s-%s-%s", result[:8], result[8:12],
 		result[12:16], result[16:20], result[20:])
+}
+
+// UUID4For converts interface to json bytes, hashes it and uses result to
+// generate an UUID4 string.
+func UUID4For(i interface{}) string {
+	hash := fnv.New128()
+	if jsdata, err := json.Marshal(i); err == nil {
+		hash.Write([]byte(jsdata))
+	} else {
+		hash.Write(make([]byte, 16))
+	}
+	return uuid4ForBytes(hash.Sum([]byte{}))
+}
+
+func UUID4() string {
+	bytes := make([]byte, 16)
+	rand.Read(bytes)
+	return uuid4ForBytes(bytes)
 }
--- common/uuid_test.go
+++ common/uuid_test.go
@@ -12,4 +12,37 @@ func TestUUID4(t *testing.T) {
 			t.Errorf("Incorrect UUID4: %s", uuid)
 		}
 	})
+
+}
+
+func TestUUID4For(t *testing.T) {
+
+	for _, tc := range []struct {
+		name string
+		item interface{}
+		want string
+	}{
+		{
+			name: "for string",
+			item: "some string a very long string whatever whatever blah",
+			want: "b9d1969b-af0c-4f63-9dbe-cb02cf64ee57",
+		},
+		{
+			name: "empty string",
+			item: "",
+			want: "0880945b-2dab-4be9-9aa0-733055270e4d",
+		},
+		{
+			name: "nil interface",
+			item: nil,
+			want: "66ab441a-fa75-4277-b806-e89c611ded0e",
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			result := UUID4For(tc.item)
+			if result != tc.want {
+				t.Errorf("Expected UUID4For(%v) = %s, but got %s", tc.item, tc.want, result)
+			}
+		})
+	}
 }
